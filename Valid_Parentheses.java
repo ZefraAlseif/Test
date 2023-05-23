6b0d2ab8ea00152b2cb037621086c0039a9fdc18
ZefraAlseif <carlosotero9819@gmail.com>
2023-05-22 21:54:44 -0400

    Deleted a Java class and added a new coding challenge.

---
+++ Valid_Parentheses.java
@@ -0,0 +1,66 @@
+/* "Works for all Cases"
+boolean result = true;
+        LinkedList<String> list = new LinkedList<String>();
+        for (int i = 0; i<s.length();i++){
+            list.add(s.substring(i, i+1));
+        }
+        HashMap<String,String> vp = new HashMap<String,String>();
+        vp.put("(",")");
+        vp.put("[","]");
+        vp.put("{","}");
+        while (!list.isEmpty()){
+            String temp = list.get(0);
+            if (list.contains(vp.get(temp))){
+                list.remove(temp);
+                list.remove(vp.get(temp));
+            }
+            else {
+                result = false;
+                break;
+            }
+        }
+        return result;
+    }
+*/
+
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Scanner;
+
+// Specific case of the parenthese being in the next index
+public class Valid_Parentheses {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        int T = sc.nextInt();
+        while (T>0){
+            String temp = sc.next();
+            System.out.println(isValid(temp));
+        }
+    }
+
+    public static boolean isValid(String s) {
+        boolean result = true;
+        LinkedList<String> list = new LinkedList<String>();
+        for (int i = 0; i<s.length();i++){
+            list.add(s.substring(i, i+1));
+        }
+        HashMap<String,String> vp = new HashMap<String,String>();
+        vp.put("(",")");
+        vp.put("[","]");
+        vp.put("{","}");
+        while (!list.isEmpty()){
+            String temp = list.get(0);
+            if (list.get(1).equals(vp.get(temp))){
+                list.remove(temp);
+                list.remove(vp.get(temp));
+            }
+            else {
+                result = false;
+                break;
+            }
+        } 
+        return result;
+    }
+}
