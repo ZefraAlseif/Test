15ebcbc332d30d2e8331f3de3f6fbcbab2234d28
ZefraAlseif <carlosotero9819@gmail.com>
2023-06-08 20:25:35 -0400

    Added the hand and card classes

--- BlackJack Game/main.py
+++ BlackJack Game/main.py
@@ -1,4 +1,13 @@
 import random
+class Card:
+    def __init__(self,suit,rank):
+        self.suit = suit
+        self.rank = rank
+    def __str__(self):
+        return f"{self.rank['rank']} of {self.suit}"
+
+
+
 class Deck:
     def __init__(self) -> None:
         suits = ["spades","clubs","hearts","diamonds"]
@@ -16,11 +25,11 @@ class Deck:
                     {"rank": "J", "value": 10},
                     {"rank": "Q", "value": 10},
                     {"rank": "K", "value": 10},
-                    ]
+                ]
         self.cards = []
         for suit in suits:
             for rank in ranks:
-                self.cards.append([suit, rank])
+                self.cards.append(Card(suit,rank))
 
     def shuffle(self):
         if len(self.cards) > 1:
@@ -34,12 +43,44 @@ class Deck:
                 self.cards_dealt.append(card)
         return self.cards_dealt
     
-deck1 = Deck()
-deck2 = Deck()
-deck2.shuffle()
-print(deck1.cards)
-print(deck2.cards)
-
-
-        
+class Hand:
+    def __init__(self,dealer = False):
+        self.cards = []
+        self.value = 0
+        self.dealer = dealer
     
+    def add_card(self, card_list):
+        self.cards.extend(card_list)
+    
+    def calculate_value(self):
+        self.value = 0
+        has_ace = False
+        for card in self.cards:
+            card_value = int(card.rank["value"])
+            self.value += card_value
+            if card.rank["rank"] == "A":
+                has_ace = True
+        if has_ace and self.value > 21:
+            self.value -=10
+    
+    def get_value(self):
+        self.calculate_value()
+        return self.value
+    
+    def is_blackjack(self):
+        return self.get_value() == 21
+    
+    def display(self):
+        print(f'''{"Dealer's" if self.dealer else "Your"} hand: ''')
+        for card in self.cards:
+            print(card)
+        
+        if not self.dealer:
+            print("Value: ", self.get_value()) 
+        print()      
+deck = Deck()
+deck.shuffle()
+
+hand = Hand()
+hand.add_card(deck.deal(2))  
+hand.display()  
